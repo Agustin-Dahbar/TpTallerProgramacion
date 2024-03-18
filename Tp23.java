3bb9b13034485e7c83c3cbfb94ec63a930b53fc9
Agustin Dahbar <agustindahbarpp@hotmail.com>
2024-03-18 18:07:56 -0300

    Añado TP 23 Y 24

--- Tp23.java
+++ Tp23.java
@@ -12,12 +12,13 @@ public class Tp23 {
 		System.out.println("Por favor ingresa un numero");
 		int numero = scanner.nextInt();
 		
-		//Bucle que itera por el multiplicador/factor para realizar las 10 primeras múltiplicaciones con el numero ingresado por el user
+		//Bucle que itera por el multiplicador/factor para realizar las 10 primeras múltiplicaciones con el numero ingresado por el user.
+		//la variable de ámbito privada del bucle for representará a este multiplicador, su primer valor será 1 como indica su inicialización y el último 10.
 		for(int i = 1; i <= 10; i++) 
 		{
 			int resultado = numero * i; //Multiplicamos al numero por el primer multiplicador. 
 			System.out.println("Los resultados de los primeros 10 múltiplos del numero son: " + resultado);
-			//En cada iteración el multiplciador aumentará un valor. Su último valor será 10
+			//En cada iteración el multiplciador aumentará un valor. 
 		}
 	}
 
