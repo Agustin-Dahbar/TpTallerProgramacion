623e2f962e2153bef1f628bec233f84cb0078538
Agustin Dahbar <agustindahbarpp@hotmail.com>
2024-04-14 00:51:51 -0300

    Arreglo algo que habia arruinado inconscientemente

--- Tp39Mejorado.java
+++ Tp39Mejorado.java
@@ -18,10 +18,10 @@ public class Tp39Mejorado {
         int numeroJugadores = scanner.nextInt(); //Variable que tendrá la cantidad de jugadores.
         
       //Bucle que se repetirá si el usuario no ingreso al menos 2 participantes.
-        while (numeroJugadores < 2) { 
+        while (numeroJugadores < 2) 
+        { 
             System.out.println("Datos inválidos. Deben ser al menos 2.");
             numeroJugadores = scanner.nextInt();
-            scanner.nextLine(); // Limpiamos el buffer
         }
         //Uso dos entradas y no repito la primera para poder tener 2 salidas de texto diferentes. La instrucción inicial y la respuesta al mal ingreso. Si reducieramos el código se repetiria la misma entrada dentro del while.
         
@@ -29,6 +29,7 @@ public class Tp39Mejorado {
         int puntajeTotalDelJugador = 0; //Variable que almacenará la puntuación total de cada jugador.
         //Creamos un BUCLE FOR EXTERNO que iterará por los jugadores (es decir, una vez por cada jugador). Para así poder darles un nombre a cada uno y analizar el puntaje de cada uno 
         //para identificar si es el mayor hasta ahora o no.
+        scanner.nextLine();
         for (int i = 0; i < numeroJugadores; i++) //Para iterar por la misma cantidad de veces que otra cosa debemos usar < si lo comparamos con 0, y <= si lo comparamos con 1. En este caso < ya que i vale 0. 
         {
             System.out.println("Ingrese el nombre del jugador " + (i + 1) + ":"); //Sumamos uno para obtener el número deseado, recordemos que i vale 0.
@@ -37,18 +38,22 @@ public class Tp39Mejorado {
            //Creamos un FOR INTERNO que iterará por los tiros de cada jugador (siempre serán 3). Obtendremos el valor de cada tiro para sumarlos a los 3 y saber su puntaje total.
             for (int j = 0; j < 3; j++) 
             {
-                System.out.println("Ingrese la distancia del tiro " + (j + 1) + " del jugador " + nombre + ":"); // Le debemos sumar uno a J ya que se inicializó con 0 en su declaración.
+                System.out.println("Ingrese la distancia del tiro de " + nombre); // Le debemos sumar uno a J ya que se inicializó con 0 en su declaración.
                 int distancia = scanner.nextInt(); //Indicamos la distancia del tiro con respecto al centro. Este valor entrado por el usuario será el argumento de una función.
                 scanner.nextLine(); // Limpiamos el buffer
 
                 int puntajePorTiro = calcularPuntaje(distancia); //Obtenemos el puntaje d cada tiro mediante la función argumentada con la distancia ingresada anteriormente x el usuario.
                 puntajeTotalDelJugador += puntajePorTiro; //Sumamos los 3 puntajes de los tiros para obtener el puntaje total del jugador.
-
+                
                 if (distancia == 0) //Sentencia IF que identifica los tiros al centro. 
                 {
                     tirosAlCentro++; //Si se ejecuta sumamos un tiro al centro. ++ == +1
                 }
             }
+            
+            System.out.println("El puntaje total de " + nombre + " fue de " + puntajeTotalDelJugador);
+            
+            System.out.println(""); //Para separación en las impresiones de la consola.
 
             //Habiendo obtenido el puntajeTotalDelJugador volvemos al bucle externo para verificar si es la máxima puntuación hasta ahora comparandola con otros puntajeTotalDeJugador..
             if (puntajeTotalDelJugador > puntajeMaximo) 
@@ -56,6 +61,8 @@ public class Tp39Mejorado {
                 puntajeMaximo = puntajeTotalDelJugador; //Actualizamos el puntaje máximo.
                 nombreGanador = nombre; //Actualizamos el nombre del actual ganador.
             }
+            
+            puntajeTotalDelJugador = 0; //Reiniciamos el puntaje para que cuando ahora comience la siguiente iteración del bucle externo, es decir la siguiente iteración de jugador, no tenga el puntaje del anterior cargado.
         }
 
         //En los bucles hay variables privadas que se declararon allí que son trascendentales para la obtención de los valores que tendrán las variables de salida finales.
@@ -69,8 +76,8 @@ public class Tp39Mejorado {
         //necesita diferentes repeticiones. Es por eso además que seleccione el bucle for, el ideal para este caso, especialmente para el interno donde se sabe que solo serán 3 ciclos.
         
         //Ya fuera de los bucles devolvemos la información obtenida en ellos (puntajeMaximo y nombreGanador).
-        System.out.println("El ganador del torneo es: " + nombreGanador + " con un puntaje de " + puntajeMaximo);
-        System.out.println("La cantidad total de tiros al centro es: " + tirosAlCentro);
+        System.out.println("El ganador del torneo fue " + nombreGanador + " con un puntaje de " + puntajeMaximo);
+        System.out.println("La cantidad total de tiros al centro fue de " + tirosAlCentro);
         scanner.close();
     }
 
